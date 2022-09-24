280dda73c17a8c3a88e58d46a9e617995aebd2f0
fabirms <Frn#2032>
2022-09-23 22:56:52 -0400

    Parâmentros, condicionais e conversão de tipos

---
+++ DS/aula2.py
@@ -0,0 +1,9 @@
+idade = 15
+
+def verica_se_pode_dirigir(idade):
+  if idade >= 18:
+   print('Tem permissão para dirigir')
+  else:
+      print('Não tem permissão pra dirigir')
+
+      verica_se_pode_dirigir(idade);
